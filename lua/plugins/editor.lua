64336b97d8a4d8e04dc566d428457bb766961002
Byungjin Park <posquit0.bj@gmail.com>
2023-08-27 03:47:48 +0900

    Update neo-tree config

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -9,7 +9,7 @@ return {
         function()
           require("neo-tree.command").execute({
             toggle = true,
-            dir = require("lazyvim.util").get_root()
+            dir = require("lazyvim.util").get_root(),
           })
         end,
         desc = "NeoTree File Explorer (root dir)",
@@ -20,11 +20,27 @@ return {
           require("neo-tree.command").execute({
             toggle = true,
             position = "float",
-            dir = require("lazyvim.util").get_root()
+            dir = require("lazyvim.util").get_root(),
           })
         end,
         desc = "NeoTree File Explorer (root dir)",
       },
-    }
-  }
+    },
+    opts = {
+      -- Close Neo-tree if it is the last window left in the tab
+      close_if_last_window = true,
+      enable_git_status = true,
+      enable_diagnostics = true,
+      filesystem = {
+        -- 2-way binding between vim's cwd and neo-tree's root
+        bind_to_cwd = false,
+        filtered_items = {
+          -- Display differently than normal items
+          visible = true,
+          hide_dotfiles = true,
+          hide_gitignored = false,
+        },
+      },
+    },
+  },
 }
