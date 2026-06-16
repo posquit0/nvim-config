7cf4e1fef4e0dd057d3fd3df2929f68bbd301394
Byungjin Park (Claud) <posquit0.bj@gmail.com>
2026-06-16 15:19:34 +0900

    feat(terraform): replace custom spec and none-ls with official LazyVim extra (#2)

--- lua/plugins/languages/terraform.lua
+++ lua/plugins/languages/terraform.lua
@@ -1,39 +1,15 @@
+-- terraform/hcl language tooling is provided by the official
+-- lazyvim.plugins.extras.lang.terraform extra (imported in config.lazy):
+-- treesitter parsers, terraformls, tflint, terraform_fmt (conform.nvim)
+-- and terraform_validate (nvim-lint).
+--
+-- Neovim's bundled ftplugins (ftplugin/terraform.vim, sourced by
+-- ftplugin/hcl.vim) already set commentstring=# %s, but it is kept here
+-- explicitly so the behaviour is visible in this config.
 vim.api.nvim_create_autocmd("FileType", {
   pattern = { "hcl", "terraform" },
   desc = "terraform/hcl commentstring configuration",
   command = "setlocal commentstring=#\\ %s",
 })
 
-return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      if type(opts.ensure_installed) == "table" then
-        vim.list_extend(opts.ensure_installed, {
-          "terraform",
-          "hcl",
-        })
-      end
-    end,
-  },
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        terraformls = {},
-      },
-    },
-  },
-  {
-    "nvimtools/none-ls.nvim",
-    opts = function(_, opts)
-      if type(opts.sources) == "table" then
-        local null_ls = require("null-ls")
-        vim.list_extend(opts.sources, {
-          null_ls.builtins.formatting.terraform_fmt,
-          null_ls.builtins.diagnostics.terraform_validate,
-        })
-      end
-    end,
-  },
-}
+return {}
